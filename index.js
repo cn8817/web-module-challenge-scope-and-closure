2880a78e4d2c63060bc45af1e26340e3a25ab79c
cn8817 <christinenguyen1433@gmail.com>
2021-06-14 13:00:02 -0700

    completed tasks

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  the variable count is being defined inside the function in counter1 and count is defined outside the function in counter2
   2. Which of the two uses a closure? How can you tell?
-  
+  counter2 uses a closure because the function has to reach outside to get information on the variable count
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter1 would be better if we want to use count outside the function for other purposes. count2 would be better if count is declared only for the purpose of using it for that function otherwise we get an error
 */
 
 // counter1 code
@@ -62,8 +63,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 2);
 }
 
 
@@ -81,8 +82,11 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB(),
+  }
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +94,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB(),
+  }
 }
 
 
